a6de1fcbd28101080445322ceb2d1995f2eac6bc
edneykerscher <edneykerscher@gmail.com>
2023-09-20 15:01:57 -0300

    Incluido o life cycle hook

--- ngModelSimples/src/app/app.component.ts
+++ ngModelSimples/src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements DoCheck {
 
   valor: string = '';
+  tamanho: number = 0;
+
+  ngDoCheck() {
+    this.tamanho = this.valor.length;
+  }
 
 }
