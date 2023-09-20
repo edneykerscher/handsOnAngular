4567d7ad152a13614b3b2659959047007d7751b1
edneykerscher <edneykerscher@gmail.com>
2023-09-20 16:49:27 -0300

    ok

--- rotas/src/app/app-routing.module.ts
+++ rotas/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { MasterComponent } from './pages/master/master.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ReportsComponent } from './pages/reports/reports.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'master', component: MasterComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'reports', component: ReportsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
